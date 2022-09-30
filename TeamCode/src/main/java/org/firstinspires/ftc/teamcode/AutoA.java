addc3f64ce492dff5eafa269650c51ae465ede5d
Randy Milbert <rmilbert@alum.mit.edu>
2022-09-30 05:35:36 -0500

    Used motor encoders to drive robot in a straight line

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoA.java
@@ -3,28 +3,76 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 @Autonomous
 public class AutoA extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         waitForStart();
-        //run("motorBackLeft");
-        //run("motorBackRight");
-        run("motorFrontLeft");
-        //run("motorFrontRight");
+        run(false);
+        run(true);
     }
-    void run(String deviceName){
-        DcMotor motorTest = hardwareMap.get(DcMotor.class, deviceName);
-        motorTest.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorTest.setTargetPosition(10000);
-        motorTest.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motorTest.setPower(0.5);
-        while(motorTest.isBusy()) {
-            telemetry.addData(deviceName, motorTest.getCurrentPosition());
-            telemetry.update();
+    void run(boolean left){
+
+        DcMotor motorBackLeft = hardwareMap.get(DcMotor.class, "motorBackLeft");
+        DcMotor motorBackRight = hardwareMap.get(DcMotor.class, "motorBackRight");
+        DcMotor motorFrontLeft = hardwareMap.get(DcMotor.class, "motorFrontLeft");
+        DcMotor motorFrontRight = hardwareMap.get(DcMotor.class, "motorFrontRight");
+
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        if (left){
+            motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         }
-        motorTest.setPower(0);
-        motorTest.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        int targetPosition = 10000;
+
+        motorBackLeft.setTargetPosition(targetPosition);
+        motorBackRight.setTargetPosition(targetPosition);
+        motorFrontLeft.setTargetPosition(targetPosition);
+        motorFrontRight.setTargetPosition(targetPosition);
+
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        motorBackLeft.setPower(0.5);
+        motorBackRight.setPower(0.5);
+        motorFrontLeft.setPower(0.5);
+        motorFrontRight.setPower(0.5);
+
+        while(motorBackLeft.isBusy() || motorBackRight.isBusy() || motorFrontLeft.isBusy() || motorFrontRight.isBusy()) {
+
+            telemetry.addData("target position ", targetPosition);
+            
+            telemetry.addData("Motor back left :)(:", motorBackLeft.getCurrentPosition());
+            telemetry.addData("Motor back right :)(:", motorBackRight.getCurrentPosition());
+            telemetry.addData("Motor front left :)(:", motorFrontLeft.getCurrentPosition());
+            telemetry.addData("Motor front right :)(:", motorFrontRight.getCurrentPosition());
+
+            telemetry.update();
+
+        }
+
+        motorBackLeft.setPower(0);
+        motorBackRight.setPower(0);
+        motorFrontLeft.setPower(0);
+        motorFrontRight.setPower(0);
+
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
     }
 }
 
