1041ebf733fccca4585b16919923998b688f4534
Randy Milbert <rmilbert@alum.mit.edu>
2022-10-15 11:00:10 -0500

    Switched which motors were being reversed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpC.java
@@ -18,8 +18,8 @@ public class TeleOpC extends LinearOpMode {
 
         // Reverse the right side motors
         // Reverse left motors if you are using NeveRests
-        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
