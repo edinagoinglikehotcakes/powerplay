af8163462239445f986d4cb9e52e0e904c7d3fa1
Randy Milbert <rmilbert@alum.mit.edu>
2022-10-27 20:38:37 -0500

    Updated tele op D to test servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpD.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpD.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 @TeleOp
@@ -10,17 +11,26 @@ public class TeleOpD extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        DcMotor hexMotor = hardwareMap.dcMotor.get("hexMotor");
+        DcMotor motorLift = hardwareMap.dcMotor.get("motorLift");
         TouchSensor touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
+        Servo servoClaw = hardwareMap.get(Servo.class, "servoClaw");
 
         waitForStart();
 
         while (opModeIsActive()) {
             double power = -gamepad1.left_stick_y;
-            hexMotor.setPower(power);
+            motorLift.setPower(power);
+            if (gamepad1.y) {
+                servoClaw.setPosition(1);
+            }
+            if (gamepad1.x) {
+                servoClaw.setPosition(0);
+            }
+            double servoClawPosition = servoClaw.getPosition();
             telemetry.addData("Target Power", power);
-            telemetry.addData("Actual Power", hexMotor.getPower());
+            telemetry.addData("Actual Power", motorLift.getPower());
             telemetry.addData("Pressed", touchSensor.isPressed());
+            telemetry.addData("Servo", servoClawPosition);
             telemetry.update();
         }
     }
